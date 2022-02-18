0bf4f5b1028ef80067ea50358db29d6ecdae3210
Pudis Pussena <pudis.pus@gmail.com>
2022-02-18 14:21:23 +0700

    update js

--- intro-to-js/pudis.js
+++ intro-to-js/pudis.js
@@ -15,3 +15,22 @@ function askMyName() {
  function randomcus() {
    prompt("คุณได้รับสิทธิ์ลุ้นรางวัลทองคำจำนวน 2 บาท ทั้งหมด 100 รางวัล มูลค่ารวม 1 ล้านบาท เพียงกรอกเลขบัตรประชาชน","x-xxxx-xxxxx-xxx-x"); 
  }
+
+
+//  age button
+
+
+function askage() {
+  const age = prompt("Your birth year","since\. 19xx")
+  const year = (2022 - age)
+  
+  alert(`ยินดีด้วย คุณจ่ายภาษีให้ประเทศนี้มา ${year}  ปี`)
+}
+
+function loadProfile() {
+  const peoplename = "Natthanun"
+  const province = "Ratchaburi"
+
+  document.getElementById('my-name').innerHTML = peoplename;
+  document.getElementById('my-province').innerHTML = province;
+}
